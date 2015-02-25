e94639ff976be98f9bec7074b9e4a37c8b664056
Chris Holden <ceholden@gmail.com>
2015-02-25 14:55:33 -0500

    Remove memmap reader -- it looks to be slower than BIP reader

--- yatsm/reader.py
+++ yatsm/reader.py
@@ -225,26 +225,6 @@ def read_row_BIP(filenames, row, size, dtype):
     return _BIP_stack_reader.read_row(row)
 
 
-def read_row_BIP_memmap(filename, row, size, dtype):
-    """ Return a np.memmap view into the row of the file requested
-
-    Args:
-      filename (str): filename to read from
-      row (int): row to read
-      size (tuple): tuple of (int, int) containing the number of columns and
-        bands in the image
-      dtype (np.dtype): NumPy datatype of the image
-
-    Returns:
-      np.memmap: 2D array (nband x ncol) containing a memmap of the row of data
-
-    """
-    return np.memmap(
-        filename, dtype, 'r',
-        offset=np.dtype(dtype).itemsize * (row * size[0]) * size[1],
-        shape=size)
-
-
 class _GDALStackReader(object):
     """ Simple class to read stacks using GDAL, keeping file objects open
 
